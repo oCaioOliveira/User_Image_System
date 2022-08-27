0f6d413660abf0f9bea6f30a8af37fde099480a9
Caio César Oliveira <54439337+oCaioOliveira@users.noreply.github.com>
2022-08-27 15:47:27 -0300

    Correção de erros de digitação

--- user_image_system/tests/testes.py
+++ user_image_system/tests/testes.py
@@ -17,8 +17,7 @@ def get_list_of_users_with_filter(initial_id: int, final_id: int):
 def successful_tests(initial_id: int, final_id: int):
     response = get_list_of_users_with_filter(initial_id, final_id)
     list_users = json.loads(response.text)
-    assert response.text == """[{"name":"Caio","user_id":1},{"name":"Pedro","user_id":2},{"name":"Augusto","user_id":3},
-    {"name":"Fernando","user_id":4},{"name":"Joao","user_id":5}]"""
+    assert response.text == """[{"name":"Caio","user_id":1},{"name":"Pedro","user_id":2},{"name":"Augusto","user_id":3},{"name":"Fernando","user_id":4},{"name":"Joao","user_id":5}]"""
     assert response.status_code == 200
     assert list_users[0]["user_id"] == 1
     assert list_users[4]["user_id"] == 5
